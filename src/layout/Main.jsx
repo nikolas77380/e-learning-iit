76c03b119495aa0ceb376412662312588358ec2d
Антон Ткаченко <antontkacenko@MacBook-Air-Anton.local>
2023-11-01 22:52:13 +0200

    Create Main.jsx & import MUI

---
+++ src/layout/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import { Button } from "@mui/material";
+
+function Main() {
+  return <Grid container></Grid>;
+}
+
+export default Main;
