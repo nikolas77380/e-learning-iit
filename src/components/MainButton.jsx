5cc8288d427c6b3b58d406ce2f32cb645555a201
Anastasia <kop.anastasia27@gmail.com>
2023-11-03 21:28:51 +0200

    changed props of button

--- src/components/MainButton.jsx
+++ src/components/MainButton.jsx
@@ -1,28 +1,23 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 
-export default function MainButton({text, onClickHandler, width, height}) {
+export default function MainButton({text, onClickHandler, sx}) {
   const buttonStyles = {
     backgroundColor: '#E8511C',
     fontFamily: 'Roboto',
-    fontSize: '30px',
     color: 'white',
     borderRadius:'0',
     boxShadow:'0px 4px 4px 0px #00000040',
     fontWeight: '600',
-    lineHeight: '23px',
     textAlign: 'center',
-    letterSpacing: '0em',
-    padding:'27px',
-    height:height,
-    width:width,
+    letterSpacing: '0em'
   };
-
   return (
       <Button 
         variant="contained" 
         style={buttonStyles} 
         onClick={onClickHandler}
+        sx={sx}
         >
           {text}
       </Button>
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import { Button } from "@mui/material";
+import MainButton from "../components/MainButton";
+
 
 function Main() {
-  return <Grid container></Grid>;
+  return <Grid container>
+    <MainButton 
+        text='почати навчання прямо зараз'
+        sx={{
+          width:'410px',
+          height:'152px',
+          fontSize: '40px',
+          lineHeight: '36.5px',
+          padding:'2px'
+          }}/> 
+  </Grid>;
 }
 
 export default Main;
