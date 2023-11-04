27ebc250922a5140bdec20be97ead33351fbf972
Антон Ткаченко <antontkacenko@MacBook-Air-Anton.local>
2023-11-04 11:30:19 +0200

    test version

--- src/Pages/HeroSection.jsx
+++ src/Pages/HeroSection.jsx
@@ -1,57 +1,72 @@
 import React from "react";
 import { Typography, Grid } from "@mui/material";
 import MainButton from "../components/MainButton";
+import MainTitle from "../components/MainTitle";
 import "./style.css";
 
 const HeroSection = () => {
   return (
-    <Grid
-      container
-      className="el1-container"
-      //   sx={{ justifyContent: "space-betwin" }}
-    >
-      <Grid
-        item
-        xs={11}
-        md={11}
-        className="el1-block"
-        sx={{ marginTop: "-10rem" }}
-      >
-        <Typography variant="h2" className="el1-title">
-          <b>FRONT-END</b> <br />
-          РОЗРОБНИК З <b>0</b>
-        </Typography>
+    <>
+      <Grid container className="el1-container">
+        <Grid
+          item
+          xs={11}
+          md={11}
+          className="el1-block1"
+          sx={{
+            marginTop: {
+              xs: "-4rem",
+              md: "10rem",
+            },
+          }}
+        >
+          <MainTitle
+            text={"FRONT-END РОЗРОБНИК З 0"}
+            sx={{ fontFamily: "Aclonica", color: "#613D2A", width: "35rem" }}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={11}
+          md={12}
+          className="el1-block"
+          sx={{
+            justifyContent: "space-around",
+            flexWrap: "wrap",
+            marginTop: "15rem",
+          }}
+        >
+          <div style={{ display: "flex", alignItems: "flex-end" }}>
+            <Typography
+              variant="h1"
+              className="el1-salary-text"
+              sx={{ color: "red" }}
+            >
+              <b>199$</b>
+            </Typography>
+            <Typography
+              variant="h2"
+              className="el1-salary-text-small"
+              sx={{ color: "black" }}
+            >
+              <span className="salary-text">800$</span>
+            </Typography>
+            <hr style={{}} className="el-hr" />
+            <hr className="el-hr1" />
+          </div>
+          <MainButton
+            text="почати навчання прямо зараз"
+            sx={{
+              width: "41px",
+
+              fontSize: "40px",
+              lineHeight: "36.5px",
+              padding: "2px",
+            }}
+          />
+        </Grid>
       </Grid>
-      <Grid
-        item
-        xs={12}
-        md={12}
-        className="el1-block"
-        sx={{
-          justifyContent: "space-around",
-          flexWrap: "wrap",
-          marginTop: "25rem",
-        }}
-      >
-        <div style={{ display: "flex", alignItems: "flex-end" }}>
-          <Typography
-            variant="h1"
-            className="el1-salary-text"
-            sx={{ color: "red" }}
-          >
-            <b>199$</b>
-          </Typography>
-          <Typography
-            variant="h2"
-            className="el1-salary-text-small"
-            sx={{ color: "black" }}
-          >
-            <span className="salary-text">800$</span>
-          </Typography>
-        </div>
-        <MainButton text="Почати навчання прямо зараз" className="el1-button" />
-      </Grid>
-    </Grid>
+    </>
   );
 };
 
--- src/Pages/WhySection.jsx
+++ src/Pages/WhySection.jsx
@@ -1,50 +1,69 @@
 import React from "react";
 import { Grid } from "@mui/material";
+import MainTitle from "../components/MainTitle";
 import "./style.css";
 
 const WhySection = () => {
   return (
-    <Grid container spacing={3} className="el2-page">
-      <Grid item xs={12} md={12}>
-        <h1 className="El2h1">
-          ЧОМУ ТОБІ ПОТРІБЕН
-          <br />
-          ЦЕЙ КУРС?
-        </h1>
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <div className="block">
-          <p className="blockText">
-            Хто хоче працювати віддалено у зручному для вас графіку із
-            перспективою росту, а не просто просиджувати час з 9 до 18
-          </p>
-        </div>
-      </Grid>
+    <>
+      <Grid container spacing={3} className="el2-page">
+        <Grid
+          item
+          xs={12}
+          md={12}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <MainTitle
+            text={"ЧОМУ ТОБІ ПОТРІБЕН ЦЕЙ КУРС?"}
+            sx={{
+              fontFamily: "Montserrat",
+              color: "#FFFFFF",
+              fontSize: "70px",
+              textAlign: "center",
+              width: "55rem",
+              marginTop: "2rem",
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <div className="block">
+            <p className="blockText">
+              Хто хоче працювати віддалено у зручному для вас графіку із
+              перспективою росту, а не просто просиджувати час з 9 до 18
+            </p>
+          </div>
+        </Grid>
 
-      <Grid item xs={12} md={6}>
-        <div className="block">
-          <p className="blockText">
-            Хто більше не хоче мріяти про краще життя, а готовий брати
-            відповідальність за нього
-          </p>
-        </div>
-      </Grid>
+        <Grid item xs={12} md={6}>
+          <div className="block">
+            <p className="blockText">
+              Хто більше не хоче мріяти про краще життя, а готовий брати
+              відповідальність за нього
+            </p>
+          </div>
+        </Grid>
 
-      <Grid item xs={12} md={6}>
-        <div className="block">
-          <p className="blockText">
-            Кого не влаштовує рівень його доходу на зараз, життя "від зп до зп",
-            робота "на дядю" за копійки, і хто хоче отримувати стабільну зп у $
-          </p>
-        </div>
-      </Grid>
+        <Grid item xs={12} md={6}>
+          <div className="block">
+            <p className="blockText">
+              Кого не влаштовує рівень його доходу на зараз, життя "від зп до
+              зп", робота "на дядю" за копійки, і хто хоче отримувати стабільну
+              зп у $
+            </p>
+          </div>
+        </Grid>
 
-      <Grid item xs={12} md={6}>
-        <div className="block">
-          <p className="blockText">Хто хоче освоїти нову професію</p>
-        </div>
+        <Grid item xs={12} md={6}>
+          <div className="block">
+            <p className="blockText">Хто хоче освоїти нову професію</p>
+          </div>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
