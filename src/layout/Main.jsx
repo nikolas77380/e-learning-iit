12ce01475dd170cbe363e226d5a3680ca871d091
Anastasia <kop.anastasia27@gmail.com>
2023-11-05 12:39:15 +0200

    responsive section done, styles are taken out as variables

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,21 +2,12 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import { Button } from "@mui/material";
 import MainButton from "../components/MainButton";
-import TechnologiesSectionEl5 from "../components/TechnologiesSectionEl-5";
+import TechnologiesSection from "../components/TechnologiesSection";
 
 
 function Main() {
   return <Grid container>
-    <MainButton 
-        text='почати навчання прямо зараз'
-        sx={{
-          width:'410px',
-          height:'152px',
-          fontSize: '40px',
-          lineHeight: '36.5px',
-          padding:'2px'
-          }}/> 
-  <TechnologiesSectionEl5/>
+  <TechnologiesSection/>
   </Grid>;
 }
 
