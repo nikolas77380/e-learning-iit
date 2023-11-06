69473e22d987a4201e9378c1b69b3be61c8403b5
Антон Ткаченко <antontkacenko@MacBook-Air-Anton.local>
2023-11-06 21:00:01 +0200

    el1 content style

--- src/Pages/WhySection.jsx
+++ src/Pages/WhySection.jsx
@@ -1,48 +1,51 @@
 import React from "react";
 import { Grid } from "@mui/material";
+import MainTitle from "../components/MainTitle";
+import WhyComponents from "../components/WhyComponents";
 import "./style.css";
 
 const WhySection = () => {
+  const whyTitle = {
+    fontFamily: "Montserrat",
+    color: "#FFFFFF",
+    fontSize: "70px",
+    textAlign: "center",
+    marginTop: "2rem",
+    display: "flex",
+    textTransform: "uppercase",
+  };
   return (
-    <Grid container spacing={3} className="el2-page">
-      <Grid item xs={12} md={12}>
-        <h1 className="El2h1">
-          ЧОМУ ТОБІ ПОТРІБЕН
-          <br />
-          ЦЕЙ КУРС?
-        </h1>
+    <Grid
+      container
+      spacing={3}
+      className="why-page"
+      sx={{ display: "flex", alignContent: "center" }}
+    >
+      <Grid
+        item
+        xs={12}
+        md={12}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        <MainTitle text={"ЧОМУ ТОбІ ПОТРІБЕН ЦЕЙ КУРС?"} sx={whtTitle} />
       </Grid>
-      <Grid item xs={12} md={6}>
-        <div className="block">
-          <p className="blockText">
-            Хто хоче працювати віддалено у зручному для вас графіку із
-            перспективою росту, а не просто просиджувати час з 9 до 18
-          </p>
-        </div>
-      </Grid>
-
-      <Grid item xs={12} md={6}>
-        <div className="block">
-          <p className="blockText">
-            Хто більше не хоче мріяти про краще життя, а готовий брати
-            відповідальність за нього
-          </p>
-        </div>
-      </Grid>
-
-      <Grid item xs={12} md={6}>
-        <div className="block">
-          <p className="blockText">
-            Кого не влаштовує рівень його доходу на зараз, життя "від зп до зп",
-            робота "на дядю" за копійки, і хто хоче отримувати стабільну зп у $
-          </p>
-        </div>
-      </Grid>
-
-      <Grid item xs={12} md={6}>
-        <div className="block">
-          <p className="blockText">Хто хоче освоїти нову професію</p>
-        </div>
+      <Grid
+        container
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+          flexWrap: "wrap",
+        }}
+        xs={12}
+        md={12}
+      >
+        <WhyComponents />
       </Grid>
     </Grid>
   );
