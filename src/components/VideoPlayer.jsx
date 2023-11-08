5fb89e0508eb3e333deb041cecdae6d2c030d0aa
Anastasia <kop.anastasia27@gmail.com>
2023-11-08 19:55:33 +0200

    moved styles to the separate file

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,64 +1,21 @@
-
 import React from "react";
 import {Grid} from "@mui/material";
 import Avatar from '@mui/material/Avatar';
 import {Typography} from "@mui/material";
 import { useState } from "react";
-import Modal from '@mui/material/Modal';
-import CloseIcon from '@mui/icons-material/Close';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import ModalVide from "./ModalVideo";
+import {titleStyle, positionStyle, overlayStyle} from './styles'
 
 
 export default function VideoPlayer({videoid, img, name, position}) {
-  
-    const titleStyle = {
-        fontFamily: 'Montserrat',
-        fontSize: '.9375rem',
-        fontWeight: '700',
-        letterSpacing: '0.0188rem',
-        textAlign: 'left',
-        textTransform:'uppercase',
-        color:'white',
-        paddingTop:'3.75rem'
-    }
-
-    const positionStyle = {
-        fontFamily: 'Roboto',
-        fontSize: '0.8125rem',
-        fontWeight: '800',
-        letterSpacing: 0,
-        textAlign: 'left',
-        marginTop:'0.3125rem',
-        color:'rgba(176, 176, 176, 1)'
-    }
-
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 650,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-        position:'relative'
-      };
 
     const [autoplayNumber, setAutoplayNumber] = useState(0);
     const handleMouseOver = () => setAutoplayNumber(1);
     const handleMouseLeave = () => setAutoplayNumber(0);
 
     const [open, setOpen] = useState(false);
-    const handleOpen = () => {
-        console.log('handleOpen called');
-        setOpen(true)};
-    const handleClose = () => { 
-        console.log('handle called');
-        setOpen(false)};
-    console.log(open);
-
+    const handleOpen = () => { setOpen(true)};
+    const handleClose = () => {setOpen(false)};
 
     const videoStyle = {
         width:'320px',
@@ -66,15 +23,6 @@ export default function VideoPlayer({videoid, img, name, position}) {
         transform: autoplayNumber ? 'scale(1.05)' : 'scale(1)',
         transition: 'transform 0.2s'
     }
-
-    const overlayStyle = {
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        background: "transparent",
-      };
     
     
     return (
@@ -82,7 +30,6 @@ export default function VideoPlayer({videoid, img, name, position}) {
           style={videoStyle} 
           onMouseOver={handleMouseOver} 
           onMouseLeave={handleMouseLeave} 
-          //onClick={handleOpen}
           >
             <iframe className="video"   title="youtube video" onClick={handleOpen}
                     src={`https://www.youtube.com/embed/${videoid}?autoplay=${autoplayNumber}&mute=1&enablejsapi=0`}
@@ -97,24 +44,7 @@ export default function VideoPlayer({videoid, img, name, position}) {
                     {position}
                 </p>
             </Grid>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            > 
-                <Box sx={style}>
-                    <Button aria-label="close" color="primary" style={{position:'absolute', top:'0', right:'0'}} onClick={handleClose}>
-                        <CloseIcon />
-                    </Button>
-                    <iframe className="video"   title="youtube video"
-                            src={`https://www.youtube.com/embed/${videoid}?autoplay=1&mute=1`}
-                            frameBorder="0" allowFullScreen  width='640px' height='360px' />
-                </Box>
-            </Modal>
-         </div>
-                    
-                    
-               
+            <ModalVide videoid={videoid} open={open} handleClose={handleClose}/>
+         </div>         
     )
 }
